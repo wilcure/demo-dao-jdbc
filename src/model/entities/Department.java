2975d0d5cf023f9d06ed7c7f58767879612bbc73
Wilson Pereira <wilsonjp.dev@outlook.com>
2020-06-08 15:40:57 -0300

    Classe seller e correção das propriedades de departments

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,37 +5,37 @@ import java.io.Serializable;
 public class Department implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private Integer Id;
-	private String Name;
+	private Integer id;
+	private String name;
 	
 	public Department() {}
 
 	public Department(Integer id, String name) {
-		Id = id;
-		Name = name;
+		this.id = id;
+		this.name = name;
 	}
 
 	public Integer getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Integer id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -48,16 +48,16 @@ public class Department implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [Id=" + Id + ", Name=" + Name + "]";
+		return "Department [Id=" + id + ", Name=" + name + "]";
 	};	
 }
